ad8a9480e0d15661853fffc397500fc0c4d7b9da
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2024-11-13 21:15:02 +0900

    Update trainer.py

--- torch/trainer.py
+++ torch/trainer.py
@@ -118,13 +118,16 @@ def valid_one_epoch(model,
 @torch.inference_mode()
 def evaluate_func(  model, 
                     accelerator, 
-                    data_loader, 
-                    processor,
+                    data_loader,
+                    tokenizer
                 ):
     
     # import evaluate
     metric = evaluate.load('cer')
-        
+    
+    # processor.tokenizer
+    # or tokenizer
+    
     model.eval()
     eval_preds, eval_labels = [], []
     
@@ -139,22 +142,44 @@ def evaluate_func(  model,
                                                                 # early_stopping=True,
                                                                 use_cache=True
                                                                 )
-            outputs = accelerator.pad_across_processes(outputs, dim=1, pad_index=processor.tokenizer.pad_token_id)
+            outputs = accelerator.pad_across_processes(outputs, dim=1, pad_index=tokenizer.pad_token_id)
             outputs = accelerator.gather_for_metrics(outputs)
-            eval_preds.extend(processor.tokenizer.batch_decode(outputs, skip_special_tokens=True, normalize=True))
+            eval_preds.extend(tokenizer.batch_decode(outputs, skip_special_tokens=True))
 
             # WER, ACC
             labels = batch['labels'].detach().cpu().numpy()
-            labels[labels == -100] = processor.tokenizer.pad_token_id
-            labels = processor.tokenizer.batch_decode(labels, skip_special_tokens=True, normalize=True)
+            labels[labels == -100] = tokenizer.pad_token_id
+            labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
             eval_labels += labels
 
     # CER SCORE
-    cer_score = metric.compute(predictions=eval_preds, references=eval_labels)
+    cer_score = 100 * metric.compute(predictions=eval_preds, references=eval_labels)
+    
+    if torch.cuda.is_available() and condition_for_memory_cleanup():
+        torch.cuda.empty_cache()  # 필요할 때만 GPU 메모리 해제
+        gc.collect()
     
     return {'cer': cer_score}
 
 
+    # evaluate metric function
+    # def compute_metrics(pred):
+    #     # import evaluate
+    #     metric = evaluate.load('cer')
+
+    #     pred_ids = pred.predictions
+    #     label_ids = pred.label_ids
+
+    #     # pad_token을 -100으로 치환
+    #     label_ids[label_ids == -100] = tokenizer.pad_token_id
+
+    #     # metrics 계산 시 special token들을 빼고 계산하도록 설정
+    #     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    #     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+
+    #     cer = 100 * metric.compute(predictions=pred_str, references=label_str)
+
+    #     return {"cer": cer}
 
 
